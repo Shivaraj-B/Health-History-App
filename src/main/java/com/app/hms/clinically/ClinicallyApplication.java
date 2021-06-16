5dad63d364245488903213534749b8c88a31cc42
Shivaraj B <36156129+Shivaraj-B@users.noreply.github.com>
2021-06-16 20:26:58 +0530

    Initial code commit

---
+++ src/main/java/com/app/hms/clinically/ClinicallyApplication.java
@@ -0,0 +1,13 @@
+package com.app.hms.clinically;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicallyApplication.class, args);
+	}
+
+}
