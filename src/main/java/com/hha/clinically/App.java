11df47d83ea389bf199982045de66fda78a72194
Shivaraj B <36156129+Shivaraj-B@users.noreply.github.com>
2021-06-17 17:17:18 +0530

    Adding DB details in app properties file

--- src/main/java/com/hha/clinically/App.java
+++ src/main/java/com/hha/clinically/App.java
@@ -1,13 +1,13 @@
-package com.app.hms.clinically;
+package com.hha.clinically;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClinicallyApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClinicallyApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
